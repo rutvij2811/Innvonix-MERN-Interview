be6f2fdff3c5f110315df31c21e1eb9f7ea9f171
Rutvij Khatri <rutvij.khatri@gmail.com>
2022-07-24 13:36:26 +0530

    vercel config update

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 const connectToMongo = require("./db");
-const express = require('express')
-const cors = require('cors')
-const path = require('path');
-
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
 connectToMongo();
 
-const app = express()
-const port = process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
 require("dotenv").config();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/auth',require('./routes/auth'))
-app.use('/api/faq',require('./routes/faq'))
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/faq", require("./routes/faq"));
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.resolve(__dirname, 'client', 'build', 'index.html')))
-  app.get('/*',(req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+if (process.env.NODE_ENV === "production") {
+	app.get("/", (req, res) => {
+		app.use(
+			express.static(
+				path.resolve(__dirname, "client", "build")
+			)
+		);
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
 }
 
 app.listen(port, () => {
-  console.log(`iNotebook app listening at http://localhost:${port}`)
-})
+	console.log(`iNotebook app listening at http://localhost:${port}`);
+});
